b0b70952aaa74891c1afed50854b74e7313e7b10
Vincentbnguyen <90225857+Vincentbnguyen@users.noreply.github.com>
2023-12-03 22:22:04 -0600

    Add simple object detection

--- QuadCounter.py
+++ QuadCounter.py
@@ -1,36 +1,59 @@
 import cv2
-import pafy
-# Replace 'YOUR_YOUTUBE_URL' with the actual URL of the livestream
-youtube_url = 'https://www.youtube.com/watch?v=cNJDExqhx5o'
+import numpy as np
 
-# Get the video stream using pafy
-video = pafy.new(youtube_url).getbest(preftype="mp4")
+def update_mask(frame, mask, background, threshold=30):
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+    diff = cv2.absdiff(gray_frame, gray_background)
 
-# OpenCV VideoCapture with the YouTube stream URL
-cap = cv2.VideoCapture(video.url)
+    # create a new mask based on threshold difference
+    _, color_mask = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
 
-# Check if the video stream is opened successfully
-if not cap.isOpened():
-    print("Error: Could not open the video stream.")
-    exit()
+    # OR between the current mask and new color_mask to account for stationary people
+    updated_mask = cv2.bitwise_or(mask, color_mask)
+    return updated_mask
+
+cap = cv2.VideoCapture("QuadCam-5mins.mp4")
+
+# Snapshot background to compare stationary entities
+ret, background = cap.read()
 
 # background subtractor
-object_detector = cv2.createBackgroundSubtractorMOG2(history=300, varThreshold=10)
+object_detector = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=40)
 
-while True: # anlayze frame by frame
+total_people = 0
+
+while True: # analyze frame by frame
     ret, frame = cap.read()
+
+    border_points = [(725, 380), (1225, 380), (1800, 1000), (100, 1000)]
+
     mask = object_detector.apply(frame)
 
+    mask = update_mask(frame, mask, background)
     _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
    
-    for cnt in contours:
+    detections = []
+
+    # Part 1: Object Detection and adding object detections to array
+    for contour in contours:
         # Calculate area and remove small elements
-        area = cv2.contourArea(cnt)
-        if area > 30:
-            cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        area = cv2.contourArea(contour)
+        if area > 10:
+            x, y, w, h = cv2.boundingRect(contour)
+            centroid_x = int(x + w / 2)
+            centroid_y = int(y + h / 2)
+            # draw object detection
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3) # draw rectangle
+            cv2.circle(frame, (centroid_x, centroid_y), 3, (0, 255, 0), -1) # draw circle
+
+    # Part 2: Object tracking that uses detections to track objects    
+
+    # draw on frame
+    cv2.polylines(frame, [np.array(border_points)], isClosed=True, color=(0, 0, 255), thickness=3)
+    cv2.putText(frame, f'Total People: {0}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
@@ -41,3 +64,4 @@ while True: # anlayze frame by frame
         break
 
 cap.release()
+cv2.destroyAllWindows()
